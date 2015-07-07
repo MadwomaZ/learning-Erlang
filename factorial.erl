84b5c53f779701e3bab2d661f6dd810e0005ac15
Evgeniya Kharlamova <madwomaz@evgeny.by>
2015-07-08 00:35:19 +0300

    Modification Function factorial

--- factorial.erl
+++ factorial.erl
@@ -1,7 +1,10 @@
 -module(factorial).
 -export([factor/1]).
 
-factor(0) ->
-	1;
-factor(N) when N > 0 ->
-	N * factor(N-1).
+factor(N) ->
+	factor(N, 1).
+
+factor(N, Acc) ->
+	Acc;
+factor(N, Acc) when N > 0 ->
+	factor(N-1, N*Acc).
