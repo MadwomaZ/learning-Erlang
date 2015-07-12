4e3d7ed12cbf1fd868a03f139200e9b61ce45075
Evgeniya Kharlamova <madwomaz@evgeny.by>
2015-07-12 21:33:30 +0300

    Modification file hhfunc.erl add function map, incr and decr

--- hhfunc.erl
+++ hhfunc.erl
@@ -4,4 +4,16 @@
 one() -> 1.
 two() -> 2.
 
-add(X, Y) -> X() + Y(). //агрументы в виде fun hhfunc:one/0
+add(X, Y) -> X() + Y(). %%агрументы в виде fun hhfunc:one/0
+
+increment([]) -> [];
+increment([H|T]) -> [H+1|increment(T)].
+
+decrement([]) -> [];
+decrement([H|T]) -> [H-1|decrement(T)].
+
+map(_, []) -> [];
+map(F, [H|T]) -> [F(H)|map(F, T)].
+
+incr(X) -> X + 1.
+decr(X) -> X - 1.
