b8a4f8949cea53c9aab7c4195cf349b93c850b37
Evgeniya Kharlamova <madwomaz@evgeny.by>
2015-07-15 23:37:50 +0300

    Make file bot.erl witch function bot

---
+++ bot.erl
@@ -0,0 +1,23 @@
+-module(bot).
+-compile(export_all).
+
+bot(Msg) ->
+	case Msg of
+		"Hello!" -> "Hi! How are you?";
+		"hello" -> "Hi! How are you?";
+		"hello)" -> "Hi! How are you?";
+		"Hello." -> "Hi! How are you?";
+		"Hello" -> "Hi! How are you?";
+		"hi" -> "Hi! How are you?";
+		"Hi." -> "Hi! How are you?";
+		"Hi" -> "Hi! How are you?";
+		"Hi!" -> "Hi! How are you?";
+		"Good!"  -> "What are you doing?"
+		"Good"  -> "What are you doing?"
+		"good"  -> "What are you doing?"
+		"good!"  -> "What are you doing?"
+		"Cool!"  -> "What are you doing?"
+		"Cool"  -> "What are you doing?"
+		"cool!"  -> "What are you doing?"
+		"cool"  -> "What are you doing?"
+	end.
