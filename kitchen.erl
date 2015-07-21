8b320bd01463cf02676e371ffdc50855e779c49a
Evgeniya Kharlamova <madwomaz@evgeny.by>
2015-07-22 01:09:56 +0300

    Add file kitchen.erl

---
+++ kitchen.erl
@@ -0,0 +1,20 @@
+-module(kitchen).
+-compile(export_all).
+
+fridge(FoodList) ->
+	receive
+		{From, {store, Food}} ->
+			From ! {self(), ok},
+			fridge([Food|FoodList]);
+		{From, {take, Food}} ->
+			case lists:member(Food, FoodList) of
+				true ->
+					From ! {self(), {ok, Food}},
+					fridge(lists:delete(Food, FoodList));
+				false ->
+					From ! {self(), not_found},
+					fridge(FoodList)
+			end;
+		terminate ->
+			ok
+	end.
