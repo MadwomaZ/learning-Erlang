a96c6f6940fa5c5eb46dc718bafae240f662cf1f
Evgeniya [MadWomaZ] Kharlamova <madwomaz@evgeny.by>
2015-07-23 22:55:05 +0300

    Add function important and normal in file multiproc.erl

--- multiproc.erl
+++ multiproc.erl
@@ -6,3 +6,25 @@ sleep(T) ->
 	after T -> ok
 	end.
 
+flush() ->
+	receive
+		_ -> flush()
+	after 0 ->
+		ok
+	end.
+
+important() ->
+	receive
+		{Priory, Msg} when Priory > 10 ->
+			[Msg | important()]
+	after 0 ->
+		normal()
+	end.
+
+normal() ->
+	receive
+		{_, Msg} ->
+			[Msg | normal()]
+	after 0 ->
+		[]
+	end.
