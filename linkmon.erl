8a60b25bc4d6317485eab0bc5ce29add7e1f0dfe
Evgeniya [MadWomaZ] Kharlamova <madwomaz@evgeny.by>
2015-07-23 23:42:19 +0300

    Add function chain in file linkmon.erl

--- linkmon.erl
+++ linkmon.erl
@@ -5,3 +5,15 @@ myproc() ->
 	timer:sleep(5000),
 	exit(reason).
 
+chain(0) ->
+	receive
+		_ -> ok
+	after 2000 ->
+		exit("chain breaks")
+	end;
+chain(N) ->
+	Pid = spawn(fun() -> chain(N-1) end),
+	link(Pid),
+	receive
+		_ -> ok
+	end.
