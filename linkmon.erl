4fe764212159e5164230e3e146fb1bf58eb06e8c
Evgeniya [MadWomaZ] Kharlamova <madwomaz@evgeny.by>
2015-07-24 01:44:47 +0300

    Add function start_critic, judge and critic in file linkmon.erl

--- linkmon.erl
+++ linkmon.erl
@@ -17,3 +17,26 @@ chain(N) ->
 	receive
 		_ -> ok
 	end.
+
+start_critic() ->
+	spawn(?MODULE, critic, []).
+
+juidge(Pid, Band, Album) ->
+	Pid ! {self(), {Band, Album}},
+	receive
+		{Pid, Criticism} -> Criticism
+	after 2000 ->
+		timeout
+	end.
+
+critic() ->
+	receive
+		{From, {"Rage Against the Turning Machine", "Unit Testify"}} ->
+			From ! {self(), "They are wonderful"};	
+		{From, {"SOAD", "Memorize"}} ->
+			From ! {self(), "Fantastic!"};
+		{From, {_, _}} ->
+			From ! {self(), "So-so"}
+	end,
+	critic().
+
