ff7ebbc7136c137b077d1f3abe09996deb185f04
Evgeniya [MadWomaZ] Kharlamova <madwomaz@evgeny.by>
2015-07-25 01:30:40 +0300

    Modification function loop in file event.erl

--- app/src/event.erl
+++ app/src/event.erl
@@ -2,6 +2,20 @@
 -compile(export_all). %заменить на export([])
 -record( state, {server,
 		 name = "",
-		 to_do = 0}).
+		 to_go = 0}).
 
+loop(S = #state{server=Server, to_go = [T|Next]}) ->
+	receive
+		{Server, Ref, cancel} ->
+			Server ! {Ref, ok}
+	after T*1000 ->
+		if Next =:= [] ->
+			Server ! {done, S#state.name};
+		   Next =/= [] ->
+			loop(S#state{to_go = Next})
+		end
+	end.
 
+normalize(N) ->
+	Limit = 49*24*60*60,
+	[N rem Limit | lists:duplicate(N div Limit, Limit)].
